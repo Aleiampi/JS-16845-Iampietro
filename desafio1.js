8b82ab8461c4c9b62d03b51badd6da439fd5c1a5
Aleiampi <88169703+Aleiampi@users.noreply.github.com>
2021-09-04 00:19:09 -0300

    desafio 1

---
+++ desafio1.js
@@ -0,0 +1,8 @@
+var nombre = prompt ("ingrese su usuario");
+
+var contrasenia = prompt ("Ingrese su contraseña de 3 numeros");
+
+alert ("su usuario es " + nombre)
+
+var hintContrasenia = contrasenia/2
+console.log ("su contraseña es el doble de "+hintContrasenia+"... esta pagina no es muy segura, no?")
