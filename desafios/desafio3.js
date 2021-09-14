9731653de2a799ae39ed72aa6451676be0ff07fc
Aleiampi <88169703+Aleiampi@users.noreply.github.com>
2021-09-13 22:00:10 -0300

    correccion ejercicio 3

--- desafios/desafio3.js
+++ desafios/desafio3.js
@@ -1,17 +1,13 @@
 var elegido = "tijera";
-
 var intentos = 2;
-
 quiereJugar = prompt("Quiere jugar? si/no");
-
 while (quiereJugar == "si" && intentos > 0) {
     ingresoUsuario = prompt("piedra, papel o tijera?");
-
     if (ingresoUsuario == elegido) {
         alert("ganaste!");
+        break;
     } else {
-        intentos = intentos - 1
-        prompt("Perdiste. piedra, papel o tijera?");
+        intentos = intentos - 1;
     }
 }
 if (intentos < 1) {
